4ae240b1641141bae89cc6137dfc45e27b0e9061
JorgeCoto98 <120097112+JorgeCoto98@users.noreply.github.com>
2023-08-04 23:17:58 +0000

    Entrega

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,48 @@
-//import react into the bundle
+// Importar React y ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+// Importar los estilos en el bundle de Webpack
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+// Importar las dependencias de Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// Definir el componente SimpleCounter que muestra el contador
+function SimpleCounter(props) {
+   return (
+      <div className="contador">
+         <div className="calendario">
+            {/* Utilizar el ícono de Font Awesome */}
+            <FontAwesomeIcon icon={faClock} style={{ color: "#01f411" }} />
+         </div>
+         {/* Mostrar las cifras del contador */}
+         <div className="cuatro">{props.cuatro}</div>
+         <div className="tres">{props.tres}</div>
+         <div className="dos">{props.dos}</div>
+         <div className="uno">{props.uno}</div>
+      </div>
+   );
+}
+
+// Inicializar el contador en 0
+let counter = 0;
+
+// Definir una función que se ejecutará cada segundo (1000 milisegundos)
+setInterval(function() {
+   // Calcular las cifras del contador
+   let four = Math.floor(counter / 1000);
+   let three = Math.floor((counter % 1000) / 100);
+   let two = Math.floor((counter % 100) / 10);
+   let one = Math.floor(counter % 10);
+
+   // Renderizar el componente SimpleCounter con las cifras actualizadas
+   ReactDOM.render(
+      <SimpleCounter cuatro={four} tres={three} dos={two} uno={one} />,
+      document.querySelector("#app")
+   );
+
+   // Incrementar el contador en 1 para el siguiente segundo
+   counter++;
+}, 1000);
